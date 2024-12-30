bd69f943c53ee9c6850129a7179e2f7da8204d03
hajarhr55 <hajarhr55@gmail.com>
2024-12-30 16:35:13 -0500

    Add Sign Up and Login functionality with validation and Navbar update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Navbar from './Components/Navbar/Navbar'; // Importing Navbar
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        {/* Add Navbar to appear on all pages */}
+        <Navbar />
         <Routes>
-          {/* تعريف المسار الرئيسي لعرض صفحة Landing_Page */}
+          {/* Define the route for the Landing Page */}
           <Route path="/" element={<Landing_Page />} />
+          {/* Add routes for Sign Up and Login pages */}
+          <Route path="/signup" element={<div>Sign Up Page</div>} />
+          <Route path="/login" element={<div>Login Page</div>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,31 +1,23 @@
-// Import necessary modules from React library
-import React, { useEffect } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-// Import components for routing from react-router-dom library
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import custom Navbar component
-import Navbar from './Components/Navbar/Navbar';
-
-// Function component for the main App
-function App() {
-
-  // Render the main App component
+const Navbar = () => {
   return (
-    <div className="App">
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */}
-          <Navbar/>
-
-          {/* Set up the Routes for different pages */}
-          <Routes>
-            {/* Define individual Route components for different pages */}
-          </Routes>
-        </BrowserRouter>
-    </div>
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
 
-// Export the App component as the default export
-export default App;
+export default Navbar;
